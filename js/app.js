54ec01f83dbfe9c9e60e7aad4c9e07578d238aaa
haowr <shinedesign@protonmail.com>
2020-11-25 04:22:24 -0800

    work

--- js/app.js
+++ js/app.js
@@ -14,7 +14,34 @@ console.log("OY")
 
     app.controller('mainCtrl', ['$http', '$scope', '$timeout',  '$interval', '$window', function ($http, $scope, $timeout, $interval, $window) {
 
+        $scope.chapelOpen = true;
+        $scope.soundOpen = false;
 
+        $scope.openLightsPage = function(){
+
+            console.log("OPen")
+            if(!$scope.chapelOpen){
+
+                if( $scope.soundOpen){
+                    $scope.chapelOpen = true;
+
+                    $scope.soundOpen = false;
+
+                }
+
+            }else{
+
+                if( !$scope.soundOpen){
+                    $scope.chapelOpen = false;
+
+                    $scope.soundOpen = true;
+                    console.log("should run")
+
+                }
+
+            }
+
+        }
     }]);
 
 
