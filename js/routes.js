f0ccdd8f7f15a910c17d04b98bdb42652e1685c1
haowr <shinedesign@protonmail.com>
2021-02-10 18:50:43 -0800

    GENERAL-UI-UPDATES

--- js/routes.js
+++ js/routes.js
@@ -10,8 +10,8 @@
 
         $routeProvider
 
-        .when('/pricing', {
-            templateUrl: '../pricing.html',
+        .when('/shinebright', {
+            templateUrl: '../shinebright.html',
             controller: 'mainCtrl',
             name: "SHINE BRIGHT COACHING | HOME",
             resolve: {
@@ -20,8 +20,8 @@
                 }
             }
         })
-            .when('/server', {
-                templateUrl: '../estimate.html',
+            .when('/rose', {
+                templateUrl: '../rose.html',
                 controller: 'mainCtrl',
                 name: "SHINE BRIGHT COACHING | HOME",
                 resolve: {
@@ -30,8 +30,8 @@
                     }
                 }
             })
-            .when('/estimate', {
-                templateUrl: '../estimates.html',
+            .when('/qlh', {
+                templateUrl: '../qlh.html',
                 controller: 'mainCtrl',
                 name: "SHINE BRIGHT COACHING | HOME",
                 resolve: {
@@ -40,133 +40,13 @@
                     }
                 }
             })
-            .when('/checker', {
-                templateUrl: '../checker.html',
+            .when('/cryptowatch', {
+                templateUrl: '../cryptowatch.html',
                 controller: 'mainCtrl',
                 name: "SHINE BRIGHT COACHING | HOME",
                 resolve: {
                     init: function ($route) {
                         //console.log("index")
-                    }
-                }
-            })
-            .when('/doghub', {
-                templateUrl: '../doghub.html',
-                controller: 'mainCtrl',
-                name: "SHINE BRIGHT COACHING | HOME",
-                resolve: {
-                    init: function ($route) {
-                        //console.log("index")
-                    }
-                }
-            })
-            .when('/forms', {
-                templateUrl: '../op.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/storage', {
-                templateUrl: '../storage.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/storageprices', {
-                templateUrl: '../storageprices.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/serverprices', {
-                templateUrl: '../serverprices.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/themeprices', {
-                templateUrl: '../themeprice.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/resume', {
-                templateUrl: '../resume.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/themes', {
-                templateUrl: '../themes.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/info', {
-                templateUrl: '../info.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/server', {
-                templateUrl: '../server.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("index")
-                    }
-                }
-            })
-            .when('/sounds', {
-                templateUrl: '../sound.html',
-                name: "SHINE BRIGHT COACHING | FORMS",
-                controller: "mainCtrl",
-                controllerAs: 'main',
-                resolve: {
-                    init: function ($route) {
-                        console.log("sounds")
                     }
                 }
             })
@@ -196,17 +76,16 @@
     })
 
     app.run(['$rootScope', function ($rootScope, $routeUpdate, $routeParams) {
-        /*
-                $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        
+                $rootScope.$on('$locationChangeStart', function (event, next, current) {
                     //console.log(Auth.isLoggedIn());
-                    console.log(next.$$route.name);
-                    $rootScope.title = next.$$route.name;
+                   console.log("Oy")
                   
                     //if($routeParams.name == )
         
                 });
         
-        */
+        
     }]);
 
 }())
